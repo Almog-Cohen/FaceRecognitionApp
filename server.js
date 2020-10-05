985d52a190583d36734bd7b39fbfc6aa8e756408
Almog Cohen <almogco94@gmail.com>
2020-10-05 14:07:48 +0300

    Adding Database url

--- server.js
+++ server.js
@@ -14,11 +14,9 @@ const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'test',
-        database: 'facerecognitiondb'
+    connectionString : {
+        host: process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
@@ -33,24 +31,24 @@ app.get('/', (req, res) => {
     res.send("Website working");
 })
 
-app.post('/signin', (req, res) => {signin.handleSignin(req, res, db ,bcrypt)})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
 
 
-app.post('/register', (req, res) => {register.handleRegister(req, res, db ,bcrypt)})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 
 
-app.get('/profile/:id', (res, req) => {profile.handleProfileGet(req, res, db)})
+app.get('/profile/:id', (res, req) => { profile.handleProfileGet(req, res, db) })
 
 
 
 
-app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
-app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT ||  3001, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log('app is runing');
 })
 
